00e08610819be7798a72b9425b9c9002adac0438
rushil <rushilchilakamarri@gmail.com>
2024-12-10 17:32:18 +0530

    - improved pdf name display

--- setup.py
+++
@@ -1,9 +0,0 @@
-from cx_Freeze import setup, Executable
-
-setup(
-    name="PomodoroTimer",
-    version="1.0",
-    description="Pomodoro Timer Application",
-    options={'build_exe': {'include_files': ['logo.png', 'arrow.png']}},
-    executables=[Executable("Pomodoro.py", base="Win32GUI")]
-)
